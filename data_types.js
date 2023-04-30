ab15efcc7149f2c2d1971d0a02e8b7463561957c
oishbdh <ikechukwuarinze614@gmail.com>
2023-04-30 21:23:26 +0100

    data_types

---
+++ data_types.js
@@ -0,0 +1,19 @@
+//Number data_type
+//a = 123;
+//console.log(a);
+//b = 10 ** 3;
+//console.log(b);
+
+//modulus: It checks how many times it can fit one number into the other and returns a remainder
+//c = 10 % 3;
+//console.log(c);
+//using parenthesis you can control the order of calculation.
+//d = 2 * (4 + 6);
+//console.log(d);
+
+//strings: characters in js enclosed in single or double quotes
+a = 'hello'
+b = " James\n"
+c = "how are you bot\n"
+d = "im doing great"
+console.log(a + b + c + d + b);
--- variable.js
+++ variable.js
@@ -1,17 +1,17 @@
 /*Assigning a value to a variable and output to a console*/
 var person;
-var person = "John"
+var person = "John";
 //To inspect what is in the variable we do
 person;
 // it outputs to the console
-console.log("Hi", person)
+console.log("Hi", person);
 
-var greeting = "Hello"
-console.log(greeting, person)
+var greeting = "Hello";
+console.log(greeting, person);
 
 //Declaring a new variable
-var petDog = "Rex"
-var petCat = "Pepper"
+var petDog = "Rex";
+var petCat = "Pepper";
 
 //output to the console
 console.log(petDog);
@@ -20,13 +20,13 @@ console.log("My pet dog's name is: " + petDog);
 console.log("My pet cat's name is " + petCat);
 
 //Declaring a new variable
-var dogSound = "Woof"
-var catSound = "Purr"
+var dogSound = "Woof";
+var catSound = "Purr";
 
 //Output to console
 console.log(petDog, "Says", dogSound);
 console.log(petCat, "Says", catSound);
 
 //Reassigning value stored in catSound
-var catSound = "meow"
+var catSound = "meow";
 console.log(petCat, "now says", catSound);
