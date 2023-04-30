04304dd849c085d96ba9afa14833f6937a40623b
oishbdh <ikechukwuarinze614@gmail.com>
2023-04-30 22:03:09 +0100

    data_types

--- data_types.js
+++ data_types.js
@@ -49,3 +49,13 @@ var result1 = test1 == 0
 var result2 = test2 == 0
 console.log("Is", num1, "an even number?", result1)
 console.log("Is", num2, "an even number", result2)
+
+var now = "Now in "
+var three = 3
+var d = "D!"
+console.log(now + three + d)
+
+var counter = 0
+counter += 5
+counter += 3
+console.log(counter)
