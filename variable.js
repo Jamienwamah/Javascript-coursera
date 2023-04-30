e2230e99d6b55d6e77ffbaae52b63499f7839f36
oishbdh <ikechukwuarinze614@gmail.com>
2023-04-30 19:47:41 +0100

    Variables

--- variable.js
+++ variable.js
@@ -8,3 +8,25 @@ console.log("Hi", person)
 
 var greeting = "Hello"
 console.log(greeting, person)
+
+//Declaring a new variable
+var petDog = "Rex"
+var petCat = "Pepper"
+
+//output to the console
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is " + petCat);
+
+//Declaring a new variable
+var dogSound = "Woof"
+var catSound = "Purr"
+
+//Output to console
+console.log(petDog, "Says", dogSound);
+console.log(petCat, "Says", catSound);
+
+//Reassigning value stored in catSound
+var catSound = "meow"
+console.log(petCat, "now says", catSound);
