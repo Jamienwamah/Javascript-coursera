176365cbffcd5a2ecbab97a0df9099eefaf0aceb
oishbdh <ikechukwuarinze614@gmail.com>
2023-04-30 20:55:44 +0100

    Operators

---
+++ operators.js
@@ -0,0 +1,24 @@
+/* Arithmetic operators in js are:
+ *multiplication
+ *division
+ *addition
+ *subtraction
+ */
+
+/* Comparison Operators in js are
+ *greater than (>)
+ *less than (<)
+ *equal to (==)
+ */
+
+// using arithmetic operators
+console.log(2 + 2);
+console.log(2 + 4 + 8 + 5);
+console.log(2 * 3 * 4 * 8);
+console.log(4 / 2);
+console.log(4 - 2 - 1);
+
+//working with comparisons operators. it outputs true if conditions are met but false if conditions are not met
+console.log(1 > 2);
+console.log(9 < 10);
+console.log(10 == 10);
