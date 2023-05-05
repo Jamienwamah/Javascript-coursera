4269859b977d73e490ecce3513218bcacb8abee3
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-05 20:38:21 +0100

    Javascript

---
+++ loops.js
@@ -0,0 +1,17 @@
+//for loops
+for (var i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+//while loops
+var i = 1;
+
+while (i < 4) {
+    console.log(i)
+    
+}
+// for loops to count from 1-3
+for (var i = 1; i <= 3; i++) {  // first condition, the start count value must be assigned. It is rep as 1. The second condition is the un-count value. How many times the loop will iterate. The final part is the incrementor.
+    console.log(i)  
+}
+console.log(Go)
---
+++ nested_loops.js
@@ -0,0 +1,12 @@
+/*for (var i = 1; i <= 2; i++) {
+    for (var j = 1; j <= 5; j++) {
+        console.log("Week " +  i  + " day " + j)
+    }
+}
+*/
+for (var year = 2020; year < 2025; year++) {
+    console.log(year)
+    for (var month = 6; month < 9; month++) {
+        console.log("--------", month)
+    }
+}
