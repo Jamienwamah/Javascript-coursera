91452b4b4417f72ba12a9d817b134d1ea874c6ad
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-05 21:06:16 +0100

    javascript

--- reading_loops.js
+++ reading_loops.js
@@ -20,3 +20,4 @@ for (var i = 0; i < 7; i++) {
     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
     console.log("%c" + cubes[i], styles)
 }
+
