960e45c281a3042d1c9ba2918d13239e03dcf8cd
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-05 21:57:45 +0100

    javascript_function

---
+++ func.js
@@ -0,0 +1,23 @@
+/* function myFunc() {
+    console.log("Hey! this is javascript");
+
+}
+myFunc();*/
+
+/*function addTwoNum() {
+    var a = 10
+    var b = 20
+    var c = a + b
+    console.log(c);
+}
+addTwoNum(); */
+
+// to make functions more flexible
+
+function addTwoNum(a,b) { // the variables in parentheses are called the function parameter
+    var c = a + b
+    console.log(c);
+}
+
+addTwoNum(10,20); // the values in the parentheses are called argument
+addTwoNum(50,10);
