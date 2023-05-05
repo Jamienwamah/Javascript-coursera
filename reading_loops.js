2842375817de589a20b4c6d74ce821c8d17717a2
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-05 20:50:45 +0100

    javascript

---
+++ reading_loops.js
@@ -0,0 +1,22 @@
+/*
+Consider the following example: You work as a developer for an online store.
+
+The store is selling letter cubes for toddlers, and the entire "Shop now" section of the site is organized in a layout where each cube on sale is displayed in a simple card component, with an image of the cube, the letter it teaches, a short description, and the price.
+
+Cards are organized in rows, so that each row contains three cards - three different letters.
+
+Each card is a preview of that specific letter cube on sale, and it's also a link to an entire page, dedicated to providing more info about the cubes, their teaching value, and providing the visitor with a way to complete their checkout process.
+
+Now, here's a quick question: where would loops fit into displaying this grid of cards showcasing the letter cubes on sale?
+
+To understand just how this works, let me code a basic prototype of how this might work.
+
+Since you still don't have enough knowledge to display website layouts in browser with the help of JavaScript, for now I'll have to settle for using a simple string and the console.
+*/
+
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
