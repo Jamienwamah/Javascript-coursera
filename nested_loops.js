242833114a70670316fb2311e7c5a4c2604fb0df
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-05 20:40:32 +0100

    javascript

--- nested_loops.js
+++ nested_loops.js
@@ -4,9 +4,24 @@
     }
 }
 */
-for (var year = 2020; year < 2025; year++) {
+/*for (var year = 2020; year < 2025; year++) {
     console.log(year)
     for (var month = 6; month < 9; month++) {
         console.log("--------", month)
+    }
+}
+*/
+
+//nested loops - one inside another
+/*for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + ", " + secondNum);
+    }
+}
+*/
+
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + " times " + secondNum + " equals " + firstNum * secondNum);
     }
 }
