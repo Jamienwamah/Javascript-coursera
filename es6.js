4f9a98d0538f8fe17861f346a18d39707d5b4106
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-06 21:12:08 +0100

    es6

---
+++ es6.js
@@ -0,0 +1,5 @@
+let user = "Miranda"; // use let if the value might change in the future
+console.log(user);
+
+const user2 = "Ann" // use const if the value is fixed and might not need to change
+console.log(user2); 
