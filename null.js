e6498888fc165f75203bc3bfb249304f01f4af9e
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-06 11:13:07 +0100

    javascript

---
+++ null.js
@@ -0,0 +1,14 @@
+/* var letters = 'abc';
+//you can use the match method to search for a particular letter in the string
+letters.match(/a/); */
+
+
+var x;
+
+if(x === null) {
+  console.log("null");
+} else if(x === undefined) {
+  console.log("undefined");
+} else {
+  console.log("ok");
+}
--- throw_catch.js
+++ throw_catch.js
@@ -1,6 +1,6 @@
 // The benefit of using try catch is that even if Javascript throws an error, it will not stop the program from executing
 
-try {
+/* try {
   console.log(c + d);
 } catch (err) {
   console.log(err);
@@ -8,7 +8,7 @@ try {
   console.log("The error was saved in the error log");
 }
 console.log("Ok now it works");
-
+*/
 //console.log(c + d);
 // console.log("This line is never reached");
 
@@ -20,3 +20,34 @@ console.log("Ok now it works");
  */
 
 //throw new ReferenceError();
+
+/* function addTwoNums(a,b) {
+    try {
+        if(typeof(a) != 'number') {
+            throw new ReferenceError('the first argument is not a number')
+        } else if (typeof(b) != 'number') {
+            throw new ReferenceError('the second argument is not a number')
+        } else {
+            console.log(a + b)
+        }
+    } catch(err) {
+        console.log("Error!", err)
+    }
+}
+addTwoNums(5, "5")
+console.log("It still works") 
+
+*/
+
+/* try {
+    console.log('hello)
+    } catch(e) {
+    console.log('caught')
+    }
+*/
+
+try {
+    Number(5).toPrecision(300)
+    } catch(e) {
+    console.log("There was an error")
+    }
