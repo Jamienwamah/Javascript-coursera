000bb2470d1958591c4b1e2b814aae8c3c74a784
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-08 21:15:30 +0100

    Javascript

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -3,6 +3,19 @@ var bird = {
   canFly: true,
   hasFeathers: true
 };
+
+var fish = {
+  hasFins: true,
+  canSwim: true,
+  hasEyes: true,
+}
+
+var carnivore = {
+  canHunt: true,
+  canKill: true,
+  canSee: true,
+
+}
 var eagle1 = Object.create(bird);
 console.log("eagle ", eagle1);
 
@@ -13,3 +26,17 @@ console.log("eagle has feathers: ", eagle1.hasFeathers);
 // you can build as many object as you want using inheritance
 var dove = Object.create(bird);
 console.log("dove has wings :", dove.hasWings);
+
+var dolphin = Object.create(fish)
+console.log("dolphin can swim:", dolphin.canSwim);
+
+var dove = Object.create(bird);
+dove.canSwim = false;
+console.log("dove can swim:", dove.canSwim);
+
+var lion = Object.create(carnivore);
+console.log("Lions can kill: ", lion.canKill);
+
+var lion = Object.create(bird);
+lion.canFly = false;
+console.log("Lions can fly: ", lion.canFly);
